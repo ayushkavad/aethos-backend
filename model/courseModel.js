7f909a2e6a37abed4e5a9dcfedd651ffa494dff2
Ayush Kavad <ayushkavad@gmail.com>
2023-03-12 16:31:42 +0530

    calculate raingsAverage and ratingsQuantity

--- model/courseModel.js
+++ model/courseModel.js
@@ -41,6 +41,7 @@ const courseSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, 'Ratings must be above 1.0'],
       max: [5, 'Ratings must be below 5.0'],
+      set: (val) => Math.round(val * 10) / 10,
     },
     ratingsQuantity: {
       type: Number,
--- model/reviewModel.js
+++ model/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Course = require('./courseModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -29,13 +30,60 @@ const reviewSchema = new mongoose.Schema(
   { toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
 
+reviewSchema.index({ course: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
     path: 'user',
     select: 'name photo',
   });
-
   next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function (courseId) {
+  const stats = await this.aggregate([
+    {
+      $match: { course: courseId },
+    },
+    {
+      $group: {
+        _id: '$course',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+  // console.log(stats);
+
+  if (stats.length > 0) {
+    await Course.findByIdAndUpdate(courseId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Course.findByIdAndUpdate(Course, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
+};
+
+reviewSchema.post('save', function () {
+  // this points to current review
+  this.constructor.calcAverageRatings(this.course);
+});
+
+// findByIdAndUpdate
+// findByIdAndDelete
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.findOne();
+  // console.log(this.r);
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  // await this.findOne(); does NOT work here, query has already executed
+  await this.r.constructor.calcAverageRatings(this.r.course);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
