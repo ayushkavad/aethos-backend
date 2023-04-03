b56eb213576a2f7dc6bdcdac3ae261e91eee6a73
Ayush Kavad <ayushkavad@gmail.com>
2023-04-03 21:45:28 +0530

    refactor

--- router/mediaRouters.js
+++ router/mediaRouters.js
@@ -1,7 +1,13 @@
 const express = require('express');
-const mediaController = require('./../controllers/mediaController');
+const {
+  videosUpload,
+  videoProcess,
+  uploadFiles,
+  getAllUploads,
+  getOneUpload,
+} = require('./../controllers/mediaController');
 const authController = require('./../controllers/authController');
-const courseController = require('./../controllers/courseController');
+const { action, deleteUpload } = require('./../controllers/courseController');
 
 const router = express.Router({ mergeParams: true });
 
@@ -9,16 +15,10 @@ router.use(authController.protect, authController.restrictTo('admin'));
 
 router
   .route('/')
-  .get(mediaController.getAllUploads)
-  .post(
-    mediaController.videosUpload,
-    mediaController.videoProcess,
-    mediaController.uploadFiles
-  );
+  .get(getAllUploads)
+  .post(videosUpload, videoProcess, uploadFiles);
 
-router.route('/:id').get(mediaController.getOneUpload);
-router
-  .route('/:courseId/uploads/:id')
-  .delete(courseController.action, mediaController.deleteUpload);
+router.route('/:id').get(getOneUpload);
+router.route('/:courseId/uploads/:id').delete(action, deleteUpload);
 
 module.exports = router;
